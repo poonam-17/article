fc9582afcba40937f839ec05d29af023ce243894
poonamkadam <poonamkadam60@gmail.com>
2018-02-24 17:52:17 +0530

    add new plan branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224111013) do
+ActiveRecord::Schema.define(version: 20180224113555) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.integer "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
